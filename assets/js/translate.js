8be37bb2f0e83e850ea9932fbce0ffa15f55c24e
Pfeiffer Zoltan <41860546+pfeihun@users.noreply.github.com>
2020-08-19 17:29:21 +0200

    translate js translate function is ready

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,89 +1,66 @@
-var data ={
+var portfolio = {}
+
+portfolio.i18n = {
+//magyar adatok
     "hu":
     {
         "mainTitle" : "Pfeiffer Zoltán Szabadúszó",
         "debut" : "Bemutatkozás",
         "about" : "Rólam",
-        "work": "Munkahelyek és Iskolák",
+        "timeline": "Munkahelyek és Iskolák",
         "portfolio" : "Portfolió",
         "contact" : "Kapcsolat",
         "more" : "Több",
         "skill" : "Képpeségek",
         "hobbi" : "Hobbik és egyéb",
-        "title": "Hello Világ!",
-        "description" : "Ez egy leírás magyarul"
+        "up" : "Fel!",
+        "menu": "Menü"
     },
+//német adatok
     "de":
     {
         "mainTitle" : "Zoltán Pfeiffer Freelancer",
         "debut" : "Vorstellung",
         "about" : "Über",
-        "work" : "Arbeitsort und Schulen",
+        "timeline" : "Arbeitsort und Schulen",
         "portfolio" : "Portfolio",
         "contact" : "Kontakt",
         "more" : "Mehr",
         "skill" : "Fähigkeiten",
         "hobbi" : "Hobbis und mehr",
-        "title": "Hello Welt!",
-        "description" : "Das ist eine Abschreibung in Deutsch"
-    },
+        "up" : "Nach oben!",
+        "menu": "Menu"
+   },
+//angol adatok
     "en":
     {
         "mainTitle" : "Zoltán Pfeiffer Freelancer",
         "debut" : "Introduction",
         "about" : "About",
-        "work" : "Workplace and School",
+        "timeline" : "Workplace and School",
         "portfolio" : "Portfolio",
         "contact" : "Contact",
         "more" : "More",
         "skill" : "Skills",
         "hobbi" : "Hobbis and more",
-        "title": "Hello World!",
-        "description" : "This is a description in English"
+        "up" : "Top!",
+        "menu": "Menu"
     }
 }
 // constant declaration and queriSelectors
 const lang = document.querySelector('.langWrap');
 const link = document.querySelectorAll('a.language');
-//menu query
-const debut = document.querySelector('span.js-debut');
-const about = document.querySelector('span.js-about');
-const work = document.querySelector('span.js-timeline');
-const portfolio = document.querySelector('span.js-portfolio');
-const contact = document.querySelector('span.js-contact');
-const more = document.querySelector('span.js-more');
-const skill = document.querySelector('span.js-skill');
-const hobbi = document.querySelector('span.js-hobbi');
-//sectionTop
-const aboutSection = document.querySelector('span.js-section-about');
-const portfolioSection = document.querySelector('span.js-section-portfolio');;
-const titleE1 = document.querySelector('.js-title');
-const descE1 = document.querySelector('.js-description');
-
 //language selector button remove and add active class 
 link.forEach ( el =>{
     el.addEventListener('click', () =>{
         lang.querySelector('.active').classList.remove('active');
         el.classList.add('active');
 //Attribute changer
-        const attr = el.getAttribute('hreflang');
-// new atribute refill
-//page title
-        window.document.title = data[attr].mainTitle;
-//Menu
-        debut.textContent = data[attr].debut;
-        about.textContent = data[attr].about;
-        work.textContent = data[attr].work;
-        portfolio.textContent = data[attr].portfolio;
-        contact.textContent = data[attr].contact;
-        more.textContent = data[attr].more;
-        skill.textContent = data[attr].skill;
-        hobbi.textContent = data[attr].hobbi;
-//SectionTop
-        aboutSection.textContent = about.textContent;
-        portfolioSection.textContent = portfolio.textContent;
-        titleE1.textContent = data[attr].title;
-        descE1.textContent = data[attr].description;
+        var attr = el.getAttribute('hreflang');
+//Menu variables data change - Language element
+for ( let node of document.querySelectorAll('[data-i18n]') ) {
+  node.innerText = portfolio.i18n[attr][node.dataset.i18n];
+}
         
     });
 });
